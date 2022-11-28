2e993a6a426ff75decb9285c049f0bf4886cae58
Hilary <hilary.seselja@gmail.com>
2022-11-28 15:33:10 +1100

    Adding stepped nav functionality to ECB prototype.

--- js/prototypes.js
+++ js/prototypes.js
@@ -3,7 +3,6 @@
 $(document).ready(function () {
 
 //ECB REVISED FLOW PROTOTYPE
-    
 
     // Open & close modals
     $('#ecb-prototype .modal-trigger').on('click', function(){
@@ -37,6 +36,49 @@ $(document).ready(function () {
             $('html,body').animate({scrollTop: anchor.offset().top},'fast');
         }, 200);
     });
+
+
+    // Stepped nav functionality
+    if ( $('#ecb-prototype .stepped-navigation-wrapper').length )  {
+        
+        var active_step = 'nav-step-' + $('.step-title').attr('data-step');
+
+        $('#' + active_step).addClass('active');
+        localStorage.setItem(active_step, 'visited');
+
+        var step_titles = ["position.html", "hours.html", "pay.html", "review.html", "finalise.html"]; 
+        //host = window.location.host;
+        var  completed_steps = active_step - 1;
+
+        for (var step = 0; step < step_titles.length; step++) {
+            var step_number = step + 1,
+            step_str = 'nav-step-' + step_number;
+            var state = localStorage.getItem(step_str);
+         
+            if ( state == "visited" && !$('#' + step_str).hasClass('active') ) {
+                $('#' + step_str).addClass('completed').attr('href', '/bga-style-guide/prototypes/ecb/' + step_titles[step]);
+            }
+        }
+    }
+
+    // Reset tool on "Create new contract"
+    $('#ecb-prototype .clear-tool').on('click', function(){
+        localStorage.clear();
+    });
+
+
+    // Sticky stepped nav behaviour
+    if ($('#ecb-prototype .stepped-navigation-wrapper').length) {
+        var sticky_position = $('.stepped-navigation-wrapper .stepped-navigation-wrapper').offset();
+        
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > sticky_position.top) {
+                $('#page-header').addClass('fixed');
+            } else {
+                $('#page-header').removeClass('fixed');
+            }
+        });
+    } 
 
 
 
