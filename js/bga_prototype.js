f919b926e2f16626f622bc7a8930294473ee9c3f
blue-baron <blue-baron@users.noreply.github.com>
2023-03-15 05:28:35 +0000

    Update gh-pages to output generated at 1b20ed2

--- js/bga_prototype.js
+++ js/bga_prototype.js
@@ -7,6 +7,35 @@ if( $("#bga-prototype").length ) {
 
     console.log('bga prototype page');
 
+    // Modal functionality
+    // Empty href modal
+    $('a[href=""]').on("click", function(){
+        console.log('no link');
+        $(".modal-example").addClass("show");
+        $(".modal-overlay").addClass("show");
+        
+    });
+
+    $('a').on("click", function(){
+        
+    });
+    
+    $(".modal-example .close").on("click", function(){
+        $(".modal-example").removeClass("show");
+        $(".modal-overlay").removeClass("show");
+    });
+
+    $(".modal-overlay").on("click", function(){
+        $(".modal-example").removeClass("show");
+        $(".modal-overlay").removeClass("show");
+    });
+    
+    // Search not working modal
+    /*$(".search-bar-search").on("click", function(){
+        $(".modal-wrapper").addClass("active");
+        $(".modal-background").addClass("active");
+    });*/
+
         
 
 
